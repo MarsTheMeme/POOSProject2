35b02206dcfe96f81ddef03f92091afc17b683fa
KitK <147215922+Kitk00@users.noreply.github.com>
2025-11-09 19:13:15 -0500

    Fixed Error When Not Logged In
    when trying to go to /calendar without any token/user_data it gives error, but now it just redirects back to login

--- frontend/src/components/LoggedInName.tsx
+++ frontend/src/components/LoggedInName.tsx
@@ -1,9 +1,52 @@
+import { jwtDecode } from 'jwt-decode';
+
+interface JwtPayload
+{
+    firstName: string;
+    lastName: string;
+}
+
 function LoggedInName()
 {
     var user_data = localStorage.getItem('user_data');
     var ud = JSON.parse( user_data as string );
-    var firstName : string = ud.firstName;
-    var lastName : string = ud.lastName;
+
+    if ( !ud )
+    {
+        try
+        {
+            const token = localStorage.getItem('access_token');
+            if (token)
+            {
+                const decoded = jwtDecode<JwtPayload>(token);
+                var firstName : string = decoded.firstName;
+                var lastName : string = decoded.lastName;
+            }
+            else
+            {
+                console.error('Error decoding token:', 'No token found');
+                alert('Session has expired. Please log in again.');
+                localStorage.removeItem('token_data');
+                localStorage.removeItem('user_data');
+                window.location.href = '/';
+                return;
+            }
+        }
+        catch(e)
+        {
+            console.error('Error decoding token:', e);
+            alert('Session has expired. Please log in again.');
+            localStorage.removeItem('token_data');
+            localStorage.removeItem('user_data');
+            window.location.href = '/';
+            return;
+        }
+    }
+    else
+    {
+        var firstName : string = ud.firstName;
+        var lastName : string = ud.lastName;
+    }
 
     function doLogout(event:any) : void
     {
